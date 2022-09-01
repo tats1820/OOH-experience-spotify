4d77772ef541dae101a82ff1719a0c33bbc67232
Vale-pixel <100896964@u.icesi.edu.co>
2022-08-31 22:09:16 -0500

    Post data function.

--- index.js
+++ index.js
@@ -1,54 +1,70 @@
-const express = require('express');
-const { Server } = require('socket.io');
+const express = require("express");
+const { Server } = require("socket.io");
 const PORT = 5050; // No cambiar, es el puerto, ngrok y este puerto deben ser iguales
-const SERVER_IP = '172.30.71.152'; // Cambiar por la IP del computador
+const SERVER_IP = "172.30.71.152"; // Cambiar por la IP del computador
 
 //const os = require('os');
 //const IPaddress = os.networkInterfaces().en0[1].address;
 
 const app = express();
 app.use(express.json());
-app.use('/app', express.static('public-app'));
-app.use('/mupi', express.static('public-mupi'));
+app.use("/app", express.static("public-app"));
+app.use("/mupi", express.static("public-mupi"));
 
-const httpServer = app.listen(PORT, () => { //websocket
-    console.log(`http://${SERVER_IP}:${PORT}/app`);
-    console.log(`http://${SERVER_IP}:${PORT}/mupi`);
+const httpServer = app.listen(PORT, () => {
+  //websocket
+  console.log(`http://${SERVER_IP}:${PORT}/app`);
+  console.log(`http://${SERVER_IP}:${PORT}/mupi`);
 });
 // Run on terminal: ngrok http 5050;
 
-const io = new Server(httpServer, { path: '/real-time' });
-let validated = true;//validar de que este centrado
+const io = new Server(httpServer, { path: "/real-time" });
+let validated = true; //validar de que este centrado
 let selectedRight = false;
 let selectedLeft = false;
+//pooner los eventos que necesitamos
+io.on("connection", (socket) => {
+  // para concetar
+  console.log(socket.id);
 
-io.on('connection', socket => {// para concetar
-    console.log(socket.id);
+  socket.on("device-size", (deviceSize) => {
+    //tamaño del celular, lo escucho
+    socket.broadcast.emit("mupi-size", deviceSize);
+  });
 
-    socket.on('device-size', deviceSize => { //tamaño del celular, lo escucho
-        socket.broadcast.emit('mupi-size', deviceSize);
-    });
+  socket.on("mobile-instructions", (instructions) => {
+    console.log(instructions, validated);
+    if (instructions.rotationY < -10 && validated) {
+      socket.broadcast.emit("accion 1"); //izquierda
+      console.log("izquierda");
+      validated = false;
+      selectedLeft = true;
+    } else if (instructions.rotationY > -10 && instructions.rotationY < 10) {
+      validated = true;
+      if (selectedLeft || selectedRight) {
+        socket.broadcast.emit("next question");
+      }
+    } else if (instructions.rotationY > 10 && validated) {
+      socket.broadcast.emit("accion 2"); // derecha
+      validated = false;
+      selectedRight = true;
+    }
 
-    socket.on('mobile-instructions', instructions => {
-        console.log(instructions, validated);
-        if (instructions.rotationY < -10 && validated) {
-            socket.broadcast.emit('accion 1'); //izquierda
-            console.log('izquierda');
-            validated = false;
-            selectedLeft = true;
-        } else if (instructions.rotationY > -10 && instructions.rotationY < 10){
-            validated = true;
-            if (selectedLeft || selectedRight){
-                socket.broadcast.emit('next question');
-            }
-        } else if (instructions.rotationY > 10 && validated){
-            socket.broadcast.emit('accion 2'); // derecha
-            validated = false;
-            selectedRight = true;
-        }
-
-        socket.broadcast.emit('mupi-instructions', instructions);
-    })
+    socket.broadcast.emit("mupi-instructions", instructions);
+  });
 });
 
-
+async function posData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
+postData("http://localhost:5050/app", { mensaje: "Hola" }).then((data) => {
+  console.log(data);
+});
--- public-app/sketch.js
+++ public-app/sketch.js
@@ -1,37 +1,37 @@
 const NGROK = `https://${window.location.hostname}`;
-console.log('Server IP: ', NGROK);
-let socket = io(NGROK, { path: '/real-time' });
+console.log("Server IP: ", NGROK);
+let socket = io(NGROK, { path: "/real-time" });
 
-let controllerX, controllerY = 0;
+let controllerX,
+  controllerY = 0;
 const interactions = 2;
 let isTouched = false;
 
 function setup() {
-    frameRate(60);
-    canvas = createCanvas(windowWidth, windowHeight);
-    //canvas.style('z-index', '-1');
-    canvas.style('position', 'fixed');
-    canvas.style('top', '0');
-    canvas.style('right', '0');
-    controllerX = windowWidth / 2;
-    controllerY = windowHeight / 2;
-    background(0);
-    angleMode(DEGREES);
+  frameRate(60);
+  canvas = createCanvas(windowWidth, windowHeight);
+  //canvas.style('z-index', '-1');
+  canvas.style("position", "fixed");
+  canvas.style("top", "0");
+  canvas.style("right", "0");
+  controllerX = windowWidth / 2;
+  controllerY = windowHeight / 2;
+  background(0);
+  angleMode(DEGREES);
 
-    socket.emit('device-size', {windowWidth, windowHeight});
+  socket.emit("device-size", { windowWidth, windowHeight });
 
-    let btn = createButton("Permitir movimiento");
-	btn.mousePressed(function(){
-		DeviceOrientationEvent.requestPermission();
-	});
-
+  let btn = createButton("Permitir movimiento");
+  btn.mousePressed(function () {
+    DeviceOrientationEvent.requestPermission();
+  });
 }
 
 function draw() {
-    background(0, 5);
-    newCursor(pmouseX, pmouseY);
-    fill(255);
-    ellipse(controllerX, controllerY, 50, 50);
+  background(0, 5);
+  newCursor(pmouseX, pmouseY);
+  fill(255);
+  ellipse(controllerX, controllerY, 50, 50);
 }
 
 /*function mouseDragged() {
@@ -39,53 +39,61 @@ function draw() {
 }*/
 
 function touchMoved() {
-    switch (interactions) {
-        case 0:
-            socket.emit('mobile-instructions', { interactions, pmouseX, pmouseY });
-            background(255, 0, 0);
-            break;
-    }
+  switch (interactions) {
+    case 0:
+      socket.emit("mobile-instructions", { interactions, pmouseX, pmouseY });
+      background(255, 0, 0);
+      break;
+  }
 }
 
-function touchStarted(){
-    isTouched = true;
+function touchStarted() {
+  isTouched = true;
 }
 
-function touchEnded(){
-    isTouched = false;
+function touchEnded() {
+  isTouched = false;
 }
 
 function deviceMoved() {
-    switch (interactions) {
-        case 1:
-            socket.emit('mobile-instructions', { interactions, pAccelerationX, pAccelerationY, pAccelerationZ });
-            background(0, 255, 255);
-            break;
-        case 2:
-                socket.emit('mobile-instructions', { interactions, rotationX, rotationY});
+  switch (interactions) {
+    case 1:
+      socket.emit("mobile-instructions", {
+        interactions,
+        pAccelerationX,
+        pAccelerationY,
+        pAccelerationZ,
+      });
+      background(0, 255, 255);
+      break;
+    case 2:
+      socket.emit("mobile-instructions", {
+        interactions,
+        rotationX,
+        rotationY,
+      });
 
-            /*if (pmouseX > windowWidth || pmouseX < 0){
+      /*if (pmouseX > windowWidth || pmouseX < 0){
                 rotationX *= -1;
             }*/
-            background(0, 255, 0);
-            break;
+      background(0, 255, 0);
+      break;
 
-        case 3:
-    }
-    
+    case 3:
+  }
 }
 
 function deviceShaken() {
-    //socket.emit('mobile-instructions', 'Moved!');
-    //background(0, 255, 255);
+  //socket.emit('mobile-instructions', 'Moved!');
+  //background(0, 255, 255);
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function newCursor(x, y) {
-    noStroke();
-    fill(255);
-    ellipse(x, y, 10, 10);
+  noStroke();
+  fill(255);
+  ellipse(x, y, 10, 10);
 }
