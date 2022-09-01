037e42f56fce35bc87849b1cb06fa9ad4bd7a9c9
Christian Castro <85885192+ConBlast@users.noreply.github.com>
2022-09-01 13:19:30 -0500

    Casos de preguntas con pantallas y fuentes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const { Server } = require("socket.io");
 const PORT = 5050; // No cambiar, es el puerto, ngrok y este puerto deben ser iguales
-const SERVER_IP = "172.30.56.202"; // Cambiar por la IP del computador
+const SERVER_IP = "192.168.0.14"; // Cambiar por la IP del computador
 const bodyParser = require("body-parser");
 
 //const os = require('os');
